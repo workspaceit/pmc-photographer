e8208dbe0f8e1236dc807170726bce0191e29bd0
Anik S <aniksarker17@gmail.com>
2018-02-28 12:56:16 +0600

    event photo url chnaged

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
   apiBaseUrl: 'http://localhost:8080',
   apiUrl: 'http://localhost:8080/auth/api',
   pictureUrl: 'http://localhost:8080/common/',
-  eventPhotoUrl: 'http://localhost:8080/event-images/web/',
+  eventPhotoUrl: 'http://localhost:8080/img/images/',
 
 /*  apiBaseUrl: 'http://163.53.151.3:8080',
   apiUrl: 'http://163.53.151.3:8080/auth/api',
