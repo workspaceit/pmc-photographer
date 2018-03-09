84ce78b98ebbaa26cbdaa29e4f228a92c8cee00b
wsitrafi <rafi@workspaceit.com>
2018-03-09 10:30:21 +0600

    Loca env setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,20 @@
+
+
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+import {localEnv} from './local';
+
 export const environment = {
   production: false,
   // URL of development API
-    apiBaseUrl: 'http://localhost:8080',
-    apiUrl: 'http://localhost:8080/auth/api',
-    pictureUrl: 'http://localhost:8080/common/',
-    eventPhotoUrl: 'http://localhost:8080/img/images/', // eventPhotoUrl: 'http://localhost:8080/event-images/web/',
-    publicApiBaseUrl: 'http://localhost:8080/public/api',
+    apiBaseUrl: localEnv.apiBaseUrl,
+    apiUrl: localEnv.apiUrl,
+    pictureUrl: localEnv.pictureUrl,
+    eventPhotoUrl: localEnv.eventPhotoUrl, // eventPhotoUrl: 'http://localhost:8080/event-images/web/',
+    publicApiBaseUrl: localEnv.publicApiBaseUrl,
 
 
 
