b9e4c6eefdcbd7843e5a9c2facb5df3ebfa5d6d6
Anik S <aniksarker17@gmail.com>
2018-05-11 16:15:25 +0600

    design fix

--- src/app/datamodel/event-image.ts
+++ src/app/datamodel/event-image.ts
@@ -1,10 +1,15 @@
 import {Event} from './event';
+import {Watermark} from "./watermark";
 
 export class EventImage {
   id: number;
   event: Event = null
   image:string;
+  watermark: Watermark;
+  inSlideshow: boolean;
+
   constructor(values: Object = {}) {
     Object.assign(this, values);
   }
+
 }
